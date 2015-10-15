3782fef229d2c07dac25a32e47d24f2b81548df0
Leon Lamle <llamle@gmail.com>
2015-10-15 17:21:22 -0400

    building slack clone, set up auth controller, auth service, login and registration. building users service

---
+++ app/users/users.service.js
@@ -0,0 +1,9 @@
+angular.module('angularfireSlackApp')
+  .factory('Users', function($firebaseArray, $firebaseObject, FirebaseUrl){
+
+    var usersRef = new Firebase(FirebaseUrl+'users');
+
+    var Users = {};
+
+    return Users;
+  })
