e38f1f7a514a76bf2aad82786c1939a2a6f8aad5
Leon Lamle <llamle@gmail.com>
2015-10-15 18:00:30 -0400

    setting up user profiles

--- app/users/users.service.js
+++ app/users/users.service.js
@@ -2,8 +2,24 @@ angular.module('angularfireSlackApp')
   .factory('Users', function($firebaseArray, $firebaseObject, FirebaseUrl){
 
     var usersRef = new Firebase(FirebaseUrl+'users');
+    
+    var users = firebaseArray(usersRef);
 
-    var Users = {};
+    var Users = {
+      getProfile: function(uid){
+        return $firebaseObject(usersRef.child(uid));
+      },
+
+      getDisplayName: function(uid){
+        return users.$getRecord(uid).displayName;
+      },
+
+      getGravatar: function(uid){
+        return '//www.gravatar.com/avatar/' + users.$getRecord(uid).emailHash;
+      },
+
+      all: users
+    };
 
     return Users;
   })
